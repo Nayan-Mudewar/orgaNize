f5bccf448f14edc6d227b0bbad3b5bb1e40454cc
Nayan-Mudewar <nayanmudewar2002@gmail.com>
2025-09-08 17:46:28 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/github/Nayan_Mudewar/orgaNize/OrgaNizeApplication.java
@@ -0,0 +1,13 @@
+package com.github.Nayan_Mudewar.orgaNize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrgaNizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrgaNizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/Nayan_Mudewar/orgaNize/OrgaNizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Nayan_Mudewar.orgaNize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrgaNizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
