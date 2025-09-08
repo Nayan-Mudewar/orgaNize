cd762672719a98d4a6d74340757baf60f5b9e43f
Nayan-Mudewar <nayanmudewar2002@gmail.com>
2025-09-08 19:47:46 +0530

    branch created

---
+++ src/main/java/Dtos/Task.java
@@ -0,0 +1,15 @@
+//package Dtos;
+//
+//import lombok.Data;
+//
+//import java.util.Date;
+//@Data
+//public class Task {
+//    String id;
+//    String title;
+//    String description;
+//    String status;
+//    Date dueDate;
+//    String createdBy;
+//    String assignedTo;
+//}
---
+++ src/main/java/Dtos/User.java
@@ -0,0 +1,13 @@
+//package Dtos;
+//
+//import lombok.Data;
+//
+//public class User {
+//
+//    private String id;
+//    private String name;
+//    private String email;
+//    private String password;
+//
+//
+//}
